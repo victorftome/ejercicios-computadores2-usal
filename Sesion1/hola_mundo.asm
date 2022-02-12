ad7ec6e72ad8ad3c3628869d39893c0d02708add
Víctor <victorforift@gmail.com>
2022-02-12 20:52:30 +0100

    Datasheet

--- Sesion1/hola_mundo.asm
+++ Sesion1/hola_mundo.asm
@@ -1,15 +1,19 @@
 ; Hola mundo en ensamblador (MC6809)
 
-	.area PROG (ABS)
-	.org  0x100
-cadena: .ascii "Hola, mundo."
+	.area PROG (ABS) 	; Se declara el nombre del area donde ira el código, en este caso PROG.
+						; También le hemos indicado el flag de ABS (absoluta)
+	.org  0x100	; Establecemos el punto en ensamblado en la dirección indicada.
+				; Esto lo podemos hacer ya que hemos establecido el area como ABS.
+
+cadena: .ascii "Hola, mundo." ; Almacenamos la cadena "Hola, mundo."
 	.byte	10	; 10 es CTRL-J: salto de linea
 	.byte	0	; 0 es CTRL-@: fin de cadena
 
-	.globl 	programa
+	.globl 	programa 	; Establecemos la etiqueta programa como una etiqueta compartida
+						; De esta forma se podra exportar para usar en otros .asm
 
 programa:
-	ldx 	#cadena
+	ldx 	#cadena	; Cargamos el valor almacenado en cadena ("Hola, mundo. SALTO-LINEA FIN CADENA") en registros de 16 bits.
 
 bucle:	lda	,x+
 	beq	acabar
